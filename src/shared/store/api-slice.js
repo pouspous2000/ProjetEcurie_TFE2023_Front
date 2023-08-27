c6a9cf3235a9596a828797306014a9c255356dab
Ubuntu <ubuntu@ip-172-31-36-83.ec2.internal>
2023-08-27 13:30:25 +0000

    FRONT_deploiement_[ADD]_[DEPLOIMENT FRONT OK]

--- src/shared/store/api-slice.js
+++ src/shared/store/api-slice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://172.31.36.83:3003',
+		baseUrl: 'https://34.228.210.132:3003',
 		prepareHeaders: (headers, { getState }) => {
 			headers.set('authorization', `Bearer token`)
 		},
